a7f8fa8fa029c5e10a93e96fc6523c847099549a
itgsod-philip-skalander <philip.skalander@elev.it-gymnasiet.se>
2015-10-09 15:06:57 +0200

    Added documentation
    Adderade dokumentation för programmet ovanför koden.

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,3 +1,10 @@
+# Den här funktionen skapar två objekt varav ett av objekten ärver från första objektet Rectangle.
+# Rectangle har egenskaperna width och height. Om width och height är mindre än 0 så funkar inte programmet.
+# Square ärver Rectangles egenskaper width och height. Men eftersom square alltid har lika långa sidor,
+# så gör vi om width och height till sides.
+# Area funktionerna tar width*height, för att få arean.
+# Circumference tar 2*width + 2*height, för att få omkretsen.
+
 class Rectangle
   def initialize(width:, height:)
     raise ArgumentError, "arguments must be positive" if width <= 0 or height <= 0
@@ -21,3 +28,5 @@ class Square < Rectangle
     @height = side
   end
 end
+
+
